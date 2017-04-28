5d48f8ecdfa8f57b1616654405b096266422078a
Pasi Niemi <pasi.niemi@nitorcreations.com>
2017-04-28 13:17:09 +0300

    Whitespace

--- python/setup.py
+++ python/setup.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from setuptools import setup
-  
+
 setup(name='nitor-vault',
       version='0.4',
       description='Vault for storing locally encypted data in S3 using KMS keys',
