ea28413a17f55b8a85c767929a61348506847dab
Pasi Niemi <pasi.niemi@nitorcreations.com>
2017-06-30 21:09:37 +0300

    Order import

--- python/n_vault/vault.py
+++ python/n_vault/vault.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import boto3
 import os
+import boto3
 from Crypto.Cipher import AES
 from Crypto.Util import Counter
 from botocore.exceptions import ClientError
