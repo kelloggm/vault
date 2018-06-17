633226e0dbe35a4f277b3eaeacaa9ee6411b9613
Pasi Niemi <pasi.niemi@nitorcreations.com>
2018-06-17 13:47:03 +0300

    Remove awscli unused dependency

--- python/setup.py
+++ python/setup.py
@@ -1,4 +1,4 @@
-# Copyright 20.19-20.19 Nitor Creations Oy
+# Copyright 20.20-20.20 Nitor Creations Oy
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,10 +15,10 @@ import sys
 from setuptools import setup
 
 setup(name='nitor-vault',
-      version='0.19',
+      version='0.20',
       description='Vault for storing locally encypted data in S3 using KMS keys',
       url='http://github.com/NitorCreations/vault',
-      download_url='https://github.com/NitorCreations/vault/tarball/0.19',
+      download_url='https://github.com/NitorCreations/vault/tarball/0.20',
       author='Pasi Niemi',
       author_email='pasi@nitor.com',
       license='Apache 2.0',
